8823a01bf616f1787d263cf225c462dacb6a5222
Jose Muanis <jose.muanis@nytimes.com>
2015-07-12 11:49:47 -0400

    adding virtualenv post activate

--- python/install.sh
+++ python/install.sh
@@ -1,12 +1,13 @@
-easy_install SpoofMAC
 
 # test if it's not installed already
 
-easy_install pip
-pip install virtualenv
-pip install virtualenvwrapper
+echo "Easy install needs sudo"
+sudo easy_install SpoofMAC
+sudo easy_install pip
+sudo pip install --upgrade virtualenv
+sudo pip install --upgrade virtualenvwrapper
 
-source virtualenv.zsh
+source $HOME/.dotfiles/python/virtualenv.zsh
 
 ln -sf $HOME/.dotfiles/python/postactivate.zsh $HOME/.virtualenvs/postactivate 
 ln -sf $HOME/.dotfiles/python/postdeactivate.zsh $HOME/.virtualenvs/postdeactivate 
